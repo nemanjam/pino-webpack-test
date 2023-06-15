bb71a5acac67fc9c8b64b72b39c683855d4abf5a
nemanjam <nemanja.mitic.elfak@hotmail.com>
2023-06-15 21:34:58 +0200

    add pino-pretty, it work, single transport

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-const { PinoWebpackPlugin } = require("pino-webpack-plugin");
+// const { PinoWebpackPlugin } = require("pino-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, context) => {
-    config.plugins.push(
-      new PinoWebpackPlugin({
-        transports: ['pino-pretty', `${__dirname}/modules/logger/transport.js`],
-      })
-    );
+  // webpack: (config, context) => {
+  //   config.plugins.push(
+  //     new PinoWebpackPlugin({
+  //       transports: ['pino-pretty', `${__dirname}/modules/logger/transport.js`],
+  //     })
+  //   );
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import logger from "../../modules/logger/logger";
+import { LoggerOptions, TransportTargetOptions, pino } from 'pino';
+
+
+
+
+
+const targets: TransportTargetOptions[] = [
+  { level: 'info', target: 'pino/file', options: { destination: 1 } },
+  { level: 'fatal', target: 'pino/file', options: { destination: 2 } },
+  { target: 'pino-pretty', level: 'info', options: { colorize: true } },
+];
+
+// const transportAbsolutePath = '../../modules/logger/transport.mjs';
+// targets.push({ target: transportAbsolutePath, level: 'info', options: {} });
+
+
+const destination = pino.transport({ targets });
+
+const pinoOptions: LoggerOptions = {
+  enabled: true,
+  timestamp: pino.stdTimeFunctions.isoTime,
+  formatters: {
+    bindings: () => ({
+      uuid: '123',
+      isoTime: new Date().toISOString(),
+    }),
+  },
+};
+
+const logger = pino(pinoOptions, destination);
 
 type Data = {
   name: string
@@ -10,7 +38,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  logger.info('from /api/hello')
+  logger.info({some_key: 'some value'}, 'from /api/hello')
 
 
   res.status(200).json({ name: 'John Doe' })
